647b045b7fe39b2b4dc3c281e46a0b9b9de9bb64
SirCodalot <tzubery.shoham@gmail.com>
2019-04-08 16:21:02 +0300

    added better manager interface
    same as before, but this time it uses templates to get the managers from the loader class

--- src/main/java/me/codalot/core/CodalotPlugin.java
+++ src/main/java/me/codalot/core/CodalotPlugin.java
@@ -1,17 +1,54 @@
 package me.codalot.core;
 
 import lombok.Getter;
+import me.codalot.core.managers.Manager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CodalotPlugin extends JavaPlugin {
+import java.util.List;
+
+public class CodalotPlugin extends JavaPlugin implements Manager {
 
     @Getter
     private static CodalotPlugin instance;
+
+    private List<Manager> managers;
 
     @Override
     public void onEnable() {
         instance = this;
 
+        load();
     }
 
+    @Override
+    public void onDisable() {
+        save();
+    }
+
+    /* Managers */
+
+    @Override
+    public void load() {
+        managers.forEach(Manager::load);
+    }
+
+    @Override
+    public void save() {
+        managers.forEach(Manager::save);
+    }
+
+    public void reload() {
+        save();
+        load();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Manager> T getManager() {
+        for (Manager manager : managers) {
+            try {
+                return (T) manager;
+            } catch (ClassCastException ignored) {}
+        }
+        return null;
+    }
 }
